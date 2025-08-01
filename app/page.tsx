9e79d9abcf79de45cc9ea8b5d5fee633b83771b0
Yibraleh <yibraleh987@gmail.com>
2025-08-01 14:21:29 +0300

    Fix: window is not defined error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-'use client'
+'use client';
+
 import { useEffect, useState } from 'react';
-import  WebApp  from "@twa-dev/sdk";
+import WebApp from '@twa-dev/sdk';
 
 interface UserData {
   id: number;
@@ -12,35 +13,35 @@ interface UserData {
 }
 
 export default function Home() {
+  const [userData, setUserData] = useState<UserData | null>(null);
 
-  const [userData, setUserData] = useState<UserData | null>(null)
-    useEffect(() => {
-      if (WebApp.initDataUnsafe.user as UserData) {
-        setUserData(WebApp.initDataUnsafe.user as UserData);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const user = WebApp?.initDataUnsafe?.user as UserData;
+      if (user) {
+        setUserData(user);
+        WebApp.ready(); // important for Telegram to mark app as ready
       }
-    },[])
+    }
+  }, []);
 
   return (
-   <main className="p-4">
-   {
-   userData ?
-    (
-      <>
-      <h1 className="text-2xl font-bold mb-4">User Information</h1>
-      <ul>
-        <li><strong>ID:</strong> {userData.id}</li>
-        <li><strong>First Name:</strong> {userData.first_name}</li>
-        <li><strong>Last Name:</strong> {userData.last_name}</li>
-        <li><strong>Username:</strong> {userData.username}</li>
-        <li><strong>Language Code:</strong> {userData.language_code}</li>
-        <li><strong>Is Premium:</strong> {userData.is_premium ? 'Yes' : 'No'}</li>
-      </ul>
-      </>
-    ) : 
-    (
-      <div>Loading....</div>
-    )
-      }
-   </main>
+    <main className="p-4">
+      {userData ? (
+        <>
+          <h1 className="text-2xl font-bold mb-4">User Information</h1>
+          <ul>
+            <li><strong>ID:</strong> {userData.id}</li>
+            <li><strong>First Name:</strong> {userData.first_name}</li>
+            <li><strong>Last Name:</strong> {userData.last_name}</li>
+            <li><strong>Username:</strong> {userData.username}</li>
+            <li><strong>Language Code:</strong> {userData.language_code}</li>
+            <li><strong>Is Premium:</strong> {userData.is_premium ? 'Yes' : 'No'}</li>
+          </ul>
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </main>
   );
 }
